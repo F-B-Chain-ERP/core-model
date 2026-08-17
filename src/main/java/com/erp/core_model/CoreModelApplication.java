97b5c1295c23add6862bb6fedd6c7822455d497e
dunghd <hoangdinhdung0205@gmail.com>
2026-08-17 14:27:27 +0700

    inint src

---
+++ src/main/java/com/erp/core_model/CoreModelApplication.java
@@ -0,0 +1,13 @@
+package com.erp.core_model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erp/core_model/CoreModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erp.core_model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
