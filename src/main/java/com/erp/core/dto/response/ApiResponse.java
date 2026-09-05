9f17b39d111feaf347c8199c670bf6c2c6931e6c
ChuChimN0n <lbaovu461@gmail.com>
2026-09-05 13:18:44 +0700

    hotfix

--- src/main/java/com/erp/core/dto/response/ApiResponse.java
+++ src/main/java/com/erp/core/dto/response/ApiResponse.java
@@ -24,4 +24,7 @@ public record ApiResponse<T>(
     public static <T> ApiResponse<T> error(int status, String errorCode, String message) {
         return new ApiResponse<>(status, errorCode, message, null, Instant.now());
     }
+    public static <T> ApiResponse<T> created(T data) {
+        return new ApiResponse<>(201, null, "Created", data, Instant.now());
+    }
 }
